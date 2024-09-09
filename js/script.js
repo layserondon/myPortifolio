6baf6fa9a0b7d6fb653c18b74043052c57a72fe3
Layse Rondon <155028785+layserondon@users.noreply.github.com>
2024-09-09 14:25:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('JavaScript carregado com sucesso!');
+    console.log('JavaScript carregado com êxito!');
     // Adicione mais interações aqui
 });
 
